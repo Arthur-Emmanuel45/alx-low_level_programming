32567864324189a17cb144a2e3bd124373657d65
emmanuel <arthuremmanuel45@gmail.com>
2023-03-16 15:30:37 -0700

    UNIX

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include<std.h>
+
+/**
+ *main-printing without printf or puts
+ *Return: this returns 0
+ */
+
+
+int main(void)
+{
+	puts("and that piece of art is useful\" - Dora korpar, 2015-10-19");
+
+	return (0);
+
+}
