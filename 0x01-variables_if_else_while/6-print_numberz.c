006a812a7504e89602ae8fe910e4594c238397ee
emmanuel <arthuremmanuel45@gmail.com>
2023-03-17 15:18:26 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints numbers 0-9 without using char or printf/puts,
+ * Return:This will return 0
+ */
+int main(void)
+{
+	int i = '0';
+
+	while (i <= '9')
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
