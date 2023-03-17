6ec5363c7736318fe537f1feb912a336cbc3b55a
emmanuel <arthuremmanuel45@gmail.com>
2023-03-17 15:26:42 -0700

    smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main-print alphabet in reverse order
+ * Return:This will return 0
+ */
+int main(void)
+{
+	char c = 'z';
+
+	while (c >= 'a')
+	{
+		putchar(c);
+		c--;
+	}
+	putchar('\n');
+	return (0);
+}
