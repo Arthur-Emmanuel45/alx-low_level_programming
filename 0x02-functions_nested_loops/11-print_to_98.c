11d048c340673a7a691f276b71b7aac815880d12
emmanuel <arthuremmanuel45@gmail.com>
2023-03-22 07:18:08 -0700

    print numbers to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,20 +2,24 @@
 /**
  * print_to_98-print natrual num to ninty eight
  * @n:The starting number
- * Return:n
+ * Return:void
  */
-int print_to_98(int n)
+void print_to_98(int n)
 {
 	if (n >= 98)
 	{
 		while (n > 98)
+		{
 			printf("%d, ", n--);
+		}
 		printf("%d\n", n);
 	}
 	else
 	{
 		while (n < 98)
+		{
 			printf("%d\n", n++);
+		}
 		printf("%d\n", n);
 	}
 }
