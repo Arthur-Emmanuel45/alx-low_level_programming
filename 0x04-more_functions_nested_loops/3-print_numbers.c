eb2e531004886f96486d4103d537b7437a94e65e
emmanuel <arthuremmanuel45@gmail.com>
2023-03-23 11:21:26 -0700

    print number

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,14 +5,12 @@
 */
 void print_numbers(void)
 {
-	_putchar('0''\n');
-	_putchar('1''\n');
-	_putchar('2''\n');
-	_putchar('3''\n');
-	_putchar('4''\n');
-	_putcahr('5''\n');
-	_putchar('6''\n');
-	_putchar('7''\n');
-	_putchar('8'"\n");
-	_putchar('9'"\n");
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar(num);
+		_putchar('\n');
+	}
+
 }
