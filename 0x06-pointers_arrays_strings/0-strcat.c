23c1072b8d6f680ef5d82689c82b635d394a2e2c
emmanuel <arthuremmanuel45@gmail.com>
2023-03-30 14:19:57 -0700

    function prototype

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,7 +5,7 @@
  *@src:second string to add to destt
  *Return: a pointer to the resulting string dest
  */
-char *strcat(char *dest, const char *src)
+char *_strcat(char *dest, const char *src)
 {
 	int index = 0, dest_len = 0;
 
