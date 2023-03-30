cd128461f809f163b1a30f12fb6757c18cca5a31
emmanuel <arthuremmanuel45@gmail.com>
2023-03-30 15:45:59 -0700

    arrays and pointers

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -4,14 +4,6 @@
  * @str: The string to be encoded.
  * Return: A pointer to the encoded string.
  */
-#include "holberton.h"
-
-/**
- * rot13 - Encodes a string using rot13.
- * @str: The string to be encoded.
- *
- * Return: A pointer to the encoded string.
- */
 char *rot13(char *str)
 {
 	int indx1 = 0, indx2;
