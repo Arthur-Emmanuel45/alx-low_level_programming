359166e8d4b88abf12c6454bb5c812ded5161102
emmanuel <arthuremmanuel45@gmail.com>
2023-04-05 05:57:29 -0700

    recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,9 +6,12 @@
  */
 int _strlen_recursion(char *s)
 {
+	int length = 0;
+
 	if (*s)
 	{
-		s++;
+		length++;
+		length = length + _strlen_recursion(s + 1);
 	}
-	return (_strlen_recursion(s + 1));
+	return (length);
 }
