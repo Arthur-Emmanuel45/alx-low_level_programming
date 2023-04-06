7e2d74a5e51beb5d35faeb2ee4a6be7640cd1d1c
emmanuel <arthuremmanuel45@gmail.com>
2023-04-06 04:21:57 -0700

    print program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main-print the program name
+ * @argc:the arguments count
+ * @argv:the argument
+ * Return:programe name
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s/n", argv[0]);
+}
