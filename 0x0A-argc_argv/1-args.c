2e6ae03ab7cdcce3123a4f1b17892b4cd695d922
emmanuel <arthuremmanuel45@gmail.com>
2023-04-07 16:30:59 -0700

    argument

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,23 +1,12 @@
+#include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
 /**
  * main-prints num of argument passed to a string
  * @argc:count the num of argument
  * @argv:the string to count
  * Return:the number
  */
-int main(int argc, char *argv[])
+int main(int argc, char __attribute__ ((unused)) *argv[])
 {
-	int i;
-	int sum;
-
-	if (argc > 1)
-	{
-		for (i = 1; i < argc; i++)
-		{
-			sum = sum + argv[i];
-		}
-
-		print("%d/n", sum);
-	}
+	printf("%d/n", argc);
 }
