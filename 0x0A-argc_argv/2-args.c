917215e33e4be99c4b86fc386c6df36b2e7fe18d
emmanuel <arthuremmanuel45@gmail.com>
2023-04-08 02:04:37 -0700

    argument

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main-print all argument
+ * @argc:count num of argument
+ * @argv:string of the argument
+ * Return:0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < agrc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
