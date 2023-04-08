21afb059425e597d7cc638c45e877752eb008765
emmanuel <arthuremmanuel45@gmail.com>
2023-04-08 01:11:41 -0700

    argument

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,11 +4,11 @@
  * main-prints num of argument passed to a string
  * @argc:count the num of argument
  * @argv:the string to count
- * Return:the number
+ * Return:0
  */
-int main(int argc, char __attribute__((unused)) *argv[])
+int main(int argc, char __attribute__((__unused__)) *argv[])
 {
-	printf("%d/n", argc - 1);
-	
+	printf("%d\n", argc - 1);
+
 	return (0);
 }
