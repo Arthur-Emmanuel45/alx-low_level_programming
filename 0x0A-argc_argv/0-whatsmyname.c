a72ca0f2f3f73bca40ada2c9b144218d1ae9f8e7
emmanuel <arthuremmanuel45@gmail.com>
2023-04-08 01:15:42 -0700

    argument

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,11 +4,11 @@
  * main-print the program name
  * @argc:the arguments count
  * @argv:the argument
- * Return:programe name
+ * Return:0
  */
-int main(int __attribute__((unused)) argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	printf("%s/n", argv[0]);
+	printf("%s\n", argv[0]);
 
 	return (0);
 }
