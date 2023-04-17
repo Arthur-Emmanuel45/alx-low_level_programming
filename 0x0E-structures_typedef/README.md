1e25c77a46a8ff84b067f796e3cb4524ac861622
emmanuel <arthuremmanuel45@gmail.com>
2023-04-17 13:03:44 -0700

    stuct

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This is a readMe of stuct and typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - struct dog
+ * @name:the dog name
+ * @age: the dog age
+ * @owner: the owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+};
+#endif
