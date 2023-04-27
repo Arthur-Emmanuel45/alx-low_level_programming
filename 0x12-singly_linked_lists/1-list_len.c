0a8b9b4b2624c21c3fb3df73a909c9a261d74e9b
Arthur-Emmanuel <arthuremmanuel45@gmail.com>
2023-04-27 10:31:15 -0700

    linked list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 /**
- * print_list - function that Prints all the elements of a list_t list.
+ * list_len-function that returns the number of elements in a linked list_t list
  * @h:the elements to check
- * Return: The number of nodes
+ * Return:The number of nodes
  */
 size_t list_len(const list_t *h)
 {
