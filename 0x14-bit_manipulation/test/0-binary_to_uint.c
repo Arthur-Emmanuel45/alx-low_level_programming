1f2b53dbd54604aecd96fc23e74030a56f516911
Arthur-Emmanuel <arthuremmanuel45@gmail.com>
2023-05-04 07:27:54 -0700

    bit manipulation

--- 0x14-bit_manipulation/test/0-binary_to_uint.c
+++ 0x14-bit_manipulation/test/0-binary_to_uint.c
@@ -19,6 +19,9 @@ unsigned int binary_to_uint(const char *b)
 
 	for (len -= 1; len >= 0; len--)
 	{
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
+
 		add += (b[len] - '0') * mult;
 		mult *= 2;
 	}
