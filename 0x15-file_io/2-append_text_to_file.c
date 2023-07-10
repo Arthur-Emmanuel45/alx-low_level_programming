315c185388f95364c6ab6125647cebad4d578b07
Arthur-Emmnauel45 <arthuremmanuel45@gmail.com>
2023-07-10 09:07:55 -0700

    file i/o

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -19,7 +19,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 
 	pd = open(filename, O_WRONLY | O_APPEND);
-	w = write(o, text_content, len);
+	w = write(pd, text_content, len);
 
 	if (pd == -1 || w == -1)
 		return (-1);
