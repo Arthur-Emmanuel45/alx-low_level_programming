acf82e8c4dde3fba004301f4b4f1614bfe8bd315
Arthur-Emmanuel45 <emmanuelarthur45@gmail.com>
2023-08-17 10:27:29 -0700

    C - Doubly linked lists

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,6 +1,7 @@
 #include "list.h"
 /**
- * sum_dlistint -  a function that returns the sum of all the data (n) of a list
+ * sum_dlistint -  a function that returns the sum
+ * of all the data (n) of a list
  * @head: a pointer to the head of thee list
  * Return: the sum of al the data or 0 if list is empty
  */
--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,6 +1,39 @@
 #include "list.h"
 /**
- * delete_dnodeint_at_index -  a function that deletes the node at index of a list
+ * delete_dnodeint_at_index -  a function that deletes
+ * the node at index of a list
  * @head: a pointer to the head of the list
  * @index:  is the index of the node that should be deleted.
  * Returns: 1 if it succeeded, -1 if it failed
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *tmp = *head;
+
+	if (*head == NULL)
+		return (-1);
+
+	for (; index != 0; index--)
+	{
+		if (tmp == NULL)
+			return (-1);
+		tmp = tmp->next;
+	}
+
+	if (tmp == *head)
+	{
+		*head = tmp->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+	}
+
+	else
+	{
+		tmp->prev->next = tmp->next;
+		if (tmp->next != NULL)
+			tmp->next->prev = tmp->prev;
+	}
+
+	free(tmp);
+	return (1);
+}
